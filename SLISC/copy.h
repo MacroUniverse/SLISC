93fb5e68cb045787ccfcdd2f625d8dfdff9d76cc
MacroUniverse <270174408@qq.com>
2019-12-22 20:26:07 -0600

    organize code

--- SLISC/copy.h
+++ SLISC/copy.h
@@ -3,6 +3,7 @@
 // all `container = container` should be implemented using copy
 #pragma once
 #include "compare.h"
+#include "slice_arith.h"
 
 namespace slisc {
 //  === pointer interface ===
@@ -1360,5 +1361,144 @@ inline void copy(CmobdDoub_O lhs, McooDoub_I rhs)
 //         }
 //     }
 // }
+
+DcmatDoub band(CbandDoub_IO);
+DcmatDoub_c band(CbandDoub_I);
+
+inline void copy(CbandDoub_O a, Doub_I s)
+{
+	copy(band(a), s);
+}
+
+DcmatComp band(CbandComp_IO);
+DcmatComp_c band(CbandComp_I);
+
+inline void copy(CbandComp_O a, Comp_I s)
+{
+	copy(band(a), s);
+}
+
+
+inline void copy(CbandDoub_O a, CbandDoub_I b)
+{
+#ifdef SLS_CHECK_SHAPE
+	if (a.idiag() < b.nup() || a.lda() - a.idiag() - 1 < b.nlow())
+		SLS_ERR("wrong shape!");
+#endif
+	a.reshape(b.n1(), b.nup(), b.nlow());
+	copy(band(a), band(b));
+}
+
+inline void copy(CbandComp_O a, CbandComp_I b)
+{
+#ifdef SLS_CHECK_SHAPE
+	if (a.idiag() < b.nup() || a.lda() - a.idiag() - 1 < b.nlow())
+		SLS_ERR("wrong shape!");
+#endif
+	a.reshape(b.n1(), b.nup(), b.nlow());
+	copy(band(a), band(b));
+}
+
+
+inline void copy(CbandDoub_O b, CmatDoub_I a)
+{
+#ifdef SLS_CHECK_SHAPE
+	if (!shape_cmp(a, b))
+		SLS_ERR("wrong shape!");
+#endif
+    Long N1 = a.n1(), N2 = a.n2();
+    for (Long j = 0; j < N2; ++j) {
+		SvecDoub sli_b = slice1(b.cmat(), j);
+		SvecDoub_c sli_a = slice1(a, j);
+        Long k = b.idiag() - j;
+		Long i_beg = max(Long(0), j - b.nup()), i_end = min(N1, j + b.nlow() + 1);
+        for (Long i = i_beg; i < i_end; ++i)
+            sli_b[k + i] = sli_a[i];
+    }
+}
+
+inline void copy(CmatDoub_O a, CbandDoub_I b)
+{
+#ifdef SLS_CHECK_SHAPE
+	if (!shape_cmp(a, b))
+		SLS_ERR("wrong shape!");
+#endif
+    Long N1 = a.n1(), N2 = a.n2();
+    for (Long j = 0; j < N2; ++j) {
+        Long k = b.idiag() - j;
+		Long i_beg = max(Long(0), j - b.nup()), i_end = min(N1, j + b.nlow() + 1);
+		SvecDoub sli_a = slice1(a, j);
+		SvecDoub_c sli_b = slice1(b.cmat(), j);
+		for (Long i = 0; i < i_beg; ++i)
+			sli_a[i] = 0;
+        for (Long i = i_beg; i < i_end; i++)
+            sli_a[i] = sli_b[k + i];
+		for (Long i = i_end; i < N1; ++i)
+			sli_a[i] = 0;
+    }
+}
+
+inline void copy(CbandComp_O b, CmatComp_I a)
+{
+#ifdef SLS_CHECK_SHAPE
+	if (!shape_cmp(a, b))
+		SLS_ERR("wrong shape!");
+#endif
+    Long N1 = a.n1(), N2 = a.n2();
+    for (Long j = 0; j < N2; ++j) {
+		SvecComp sli_b = slice1(b.cmat(), j);
+		SvecComp_c sli_a = slice1(a, j);
+        Long k = b.idiag() - j;
+		Long i_beg = max(Long(0), j - b.nup()), i_end = min(N1, j + b.nlow() + 1);
+        for (Long i = i_beg; i < i_end; ++i)
+            sli_b[k + i] = sli_a[i];
+    }
+}
+
+inline void copy(CmatComp_O a, CbandComp_I b)
+{
+#ifdef SLS_CHECK_SHAPE
+	if (!shape_cmp(a, b))
+		SLS_ERR("wrong shape!");
+#endif
+    Long N1 = a.n1(), N2 = a.n2();
+    for (Long j = 0; j < N2; ++j) {
+        Long k = b.idiag() - j;
+		Long i_beg = max(Long(0), j - b.nup()), i_end = min(N1, j + b.nlow() + 1);
+		SvecComp sli_a = slice1(a, j);
+		SvecComp_c sli_b = slice1(b.cmat(), j);
+		for (Long i = 0; i < i_beg; ++i)
+			sli_a[i] = 0;
+        for (Long i = i_beg; i < i_end; i++)
+            sli_a[i] = sli_b[k + i];
+		for (Long i = i_end; i < N1; ++i)
+			sli_a[i] = 0;
+    }
+}
+
+
+// void copy(Matrix<T> &b, const Matrix<T> &a, Long_I Nup, Long_I Nlow)
+// {
+//     Long N1 = a.n1(), N2 = a.n2();
+//     for (Long i = 0; i < N1; i++) {
+//         Long k = Nlow - i;
+//         for (Long j = max(Long(0), i - Nlow); j < MIN(N2, i + Nup + 1); j++) {
+//             b(i, k + j) = a(i, j);
+//         }
+//     }
+// }
+
+// template <class T, SLS_IF0(is_scalar<T>())>
+// void copy(Matrix<T> &a, const Matrix<T> &b, Long_I Nup, Long_I Nlow)
+// {
+//     Long N1 = a.n1(), N2 = a.n2();
+//     for (Long i = 0; i < N1; i++) {
+//         Long k = Nlow - i;
+//         for (Long j = max(Long(0), i - Nlow); j < MIN(N2, i + Nup + 1); j++) {
+//             a(i, j) = b(i, k + j);
+//         }
+//     }
+// }
+
 
 } // namespace slisc
