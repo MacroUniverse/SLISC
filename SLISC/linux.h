404ed8fe1dece5e02c624402b63a8a3760dc2647
MacroUniverse <270174408@qq.com>
2020-01-29 00:17:01 -0600

    add child_pid() to linux.h
    * get process id for all direct child processes

--- SLISC/linux.h
+++ SLISC/linux.h
@@ -7,6 +7,8 @@
 #include <string>
 #include <array>
 #include "global.h"
+#include <sstream>
+#include <unistd.h>
 
 namespace slisc {
 
@@ -20,6 +22,23 @@ inline Str exec_str(Str_I cmd) {
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
         result += buffer.data();
     return result;
+}
+
+// get pid of all child processes
+// reslut should be sorted (not sure)
+// return -1 if no child
+inline void child_pid(vector<Int> &child_pids, Int_I pid)
+{
+	Str cmd  = "pgrep -P " + to_string(pid);
+	std::istringstream iss(exec_str(cmd));
+	Str pid_str;
+	child_pids.clear();
+	while (true) {
+		std::getline(iss, pid_str);
+        if (iss.eof())
+            break;
+        child_pids.push_back(stoi(pid_str));
+	}
 }
 
 } // namespace slisc
--- test/test_linux.cpp
+++ test/test_linux.cpp
@@ -1,8 +1,39 @@
 #include "../SLISC/linux.h"
+#include <signal.h>
 
 void test_linux()
 {
 	using namespace slisc;
+	
+	// test exec_str()
 	if (exec_str("echo hello world!") != "hello world!\n")
 		SLS_ERR("failed!");
+	
+	// test child_pid()
+	{
+		Int child_pid1 = fork(), child_pid2;
+		if (child_pid1 < 0)
+			SLS_ERR("failed!");
+		else if (child_pid1 == 0) { // in child1 process
+			sleep(5);
+		}
+		else { // in main process
+			child_pid2 = fork();
+			if (child_pid2 < 0)
+				SLS_ERR("failed!");
+			else if (child_pid2 == 0) { // in child2 process
+				sleep(5);
+			}
+			else { // in main process
+				vector<Int> child_pids;
+				child_pid(child_pids, getpid());
+				if (child_pid1 != child_pids[0])
+					SLS_ERR("failed!");
+				if (child_pid2 != child_pids[1])
+					SLS_ERR("failed!");
+				kill(child_pid1, SIGKILL);
+				kill(child_pid2, SIGKILL);
+			}
+		}
+	}
 }
