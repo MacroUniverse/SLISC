4deab639e3594410f8d80adb9bd51be45eceb509
MacroUniverse <270174408@qq.com>
2020-03-20 17:00:44 -0500

    update

--- SLISC/copy.h
+++ SLISC/copy.h
@@ -709,6 +709,17 @@ inline void copy(VecDoub_O v, VecDoub_I v1)
     veccpy(v.ptr(), v1.ptr(), v.size());
 }
 
+inline void copy(VecDoub_O v, SvecDoub_I v1)
+{
+#ifdef SLS_CHECK_SHAPE
+    if (!shape_cmp(v, v1))
+        SLS_ERR("wrong shape!");
+#endif
+    if (v.size() == 0)
+        return;
+    veccpy(v.ptr(), v1.ptr(), v.size());
+}
+
 inline void copy(VecComp_O v, VecComp_I v1)
 {
 #ifdef SLS_CHECK_SHAPE
