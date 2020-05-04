34834198cf0d6d277c9a918fe257ed9271d3e567
MacroUniverse <270174408@qq.com>
2020-05-04 01:57:07 -0500

    amend

--- matt2matb.cpp
+++ matt2matb.cpp
@@ -1,10 +1,13 @@
 // convert .matt files into .matb files
+#define SLS_MATB_REPLACE
 #include "SLISC/matt.h"
 #include "SLISC/matb.h"
 
 int main(int argc, char *argv[])
 {
     using namespace slisc;
-	for (Long i = 1; i < argc; ++i)
+	for (Long i = 1; i < argc; ++i) {
+		cout << argv[i] << endl;
 		matt2matb(argv[i]);
+	}
 }
