d45adc15a0dd289f450ab2fa91ab2a91f043b300
MacroUniverse <270174408@qq.com>
2020-05-04 19:14:33 -0500

    amend

--- matt2matb.cpp
+++ matt2matb.cpp
@@ -5,22 +5,22 @@
 #include "SLISC/matb.h"
 #include "SLISC/time.h"
 
-int main(int argc, char *argv[])
-{
     using namespace slisc;
+Int main(Int argc, Char *argv[])
+{
 	Bool rm = false;
 	if (strcmp(argv[argc-1], "-d") == 0) {
 		rm = true;
 		--argc;
 	}
 	omp_set_num_threads(8);
-	Long N = 0;
+	Int N = 0;
 	Timer timer; timer.tic();
 #pragma omp parallel for schedule(dynamic, 3)
-	for (Long i = 1; i < argc; ++i) {
+	for (Int i = 1; i < argc; ++i) {
 #pragma omp atomic
 		++N;
-		cout << argv[i] << "  " << N << "/" << argc-1 << "  " << timer.toc() << "s" << endl;
+		printf("%s  %i/%i  %es\n", argv[i], N, argc-1, timer.toc());
 		matt2matb(argv[i]);
 		if (rm)
 			remove(argv[i]);
