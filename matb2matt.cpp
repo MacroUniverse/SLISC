6c09a913efc8ba0a0edad1f854932cf0d1b9b88b
MacroUniverse <270174408@qq.com>
2020-06-24 18:27:13 -0500

    matt2matb: ignore corrupted files, change calling syntax

--- matb2matt.cpp
+++ matb2matt.cpp
@@ -1,4 +1,5 @@
 // convert .matb files into .matt files
+// usage: `find ... -exec matb2matt [-d] {} +`
 #include <omp.h>
 #define SLS_MATB_REPLACE
 #include "SLISC/matt.h"
@@ -9,20 +10,25 @@ using namespace slisc;
 Int main(Int argc, Char *argv[])
 {
 	Bool rm = false;
-	if (strcmp(argv[argc-1], "-d") == 0) {
+	Long start = 1;
+	if (strcmp(argv[1], "-d") == 0) {
 		rm = true;
-		--argc;
+		++start;
 	}
 	omp_set_num_threads(8);
 	Int N = 0;
 	Timer timer; timer.tic();
 #pragma omp parallel for schedule(dynamic, 3)
-	for (Int i = 1; i < argc; ++i) {
+	for (Int i = start; i < argc; ++i) {
 #pragma omp atomic
 		++N;
 		printf("%s  %i/%i  %es\n", argv[i], N, argc-1, timer.toc());
 		fflush(stdout);
-		matb2matt(argv[i]);
+		try {matb2matt(argv[i]);}
+		catch (...) {
+			cout << "warning: exception found, skipping..." << endl;
+			continue;
+		}
 		if (rm)
 			remove(argv[i]);
 	}
