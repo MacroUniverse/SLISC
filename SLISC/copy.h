12e1f9def0eddfe090ddfb774b2cf18625dfd9f8
MacroUniverse <270174408@qq.com>
2021-02-28 22:00:12 -0600

    instanciate

--- SLISC/copy.h
+++ SLISC/copy.h
@@ -2164,6 +2164,23 @@ inline void copy(McooComp_O v, McooComp_I v1)
     veccpy(v.col_ptr(), v1.col_ptr(), Nnz);
 }
 
+inline void copy(McooComp_O v, McooDoub_I v1)
+{
+#ifdef SLS_CHECK_SHAPES
+    if (!shape_cmp(v, v1))
+        SLS_ERR("wrong shape!");
+    if (v.capacity() < v1.nnz())
+        SLS_ERR("not enough capacity!");
+#endif
+    if (v1.nnz() == 0)
+        return;
+    Long Nnz = v1.nnz();
+    v.resize(Nnz);
+    veccpy(v.ptr(), v1.ptr(), Nnz);
+    veccpy(v.row_ptr(), v1.row_ptr(), Nnz);
+    veccpy(v.col_ptr(), v1.col_ptr(), Nnz);
+}
+
 
 inline void copy(CmatDoub_O lhs, McooDoub_I rhs)
 {
