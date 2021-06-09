f9baeb8d8b3521379d5aebf883688b3fae191311
MacroUniverse <270174408@qq.com>
2021-06-09 18:09:00 -0500

    update input.h

--- SLISC/input.h
+++ SLISC/input.h
@@ -1,8 +1,17 @@
 #pragma once
-#define SLS_INP(var) do {cin >> var; cout << #var << " = " << var << ";    ";} while(0);
+#include "global.h"
+#define SLS_INP(var) do {cin >> var; cout << #var << " = " << var << ";    "; if (!cin.good()) SLS_ERR("illegal input file!");} while(0);
 
 namespace slisc {
-inline void cin_line()
+inline void cin_comment()
+{
+    string str;
+    cin >> str;
+    if (str.size() > 0 && str.substr(0, 2) != "//")
+        SLS_ERR("each line must only be followed by a comment starting with //");
+}
+
+inline void cin_LF()
 {
     cin.ignore(1000, '\n');
     cout << endl;
---
+++ test/test_input.cpp
@@ -0,0 +1,25 @@
+#include "../SLISC/input.h"
+
+void test_input()
+{
+	using namespace slisc;
+	Long i1, i2, i3, i4;
+	Doub d1, d2, d3;
+	Str s1, s2;
+
+	cin_comment(); cin_LF();
+	cin_LF();
+	cin_comment(); cin_LF();
+	SLS_INP(i1); SLS_INP(i2); SLS_INP(d1); cin_comment(); cin_LF();
+	SLS_INP(d2); SLS_INP(s1); SLS_INP(i3); cin_comment(); cin_LF();
+	cin_LF();
+	cin_comment(); cin_LF();
+	SLS_INP(d3); SLS_INP(i4); SLS_INP(s2); cin_comment(); cin_LF();
+
+	if (i1 != 123 || i2 != 234 || d1 != 345.6)
+		SLS_ERR("failed!");
+	if (d2 != 456.7 || s1 != "a567" || i3 != 678)
+		SLS_ERR("failed!");
+	if (d3 != 789.1 || i4 != 890 || s2 != "yoyo")
+		SLS_ERR("failed!");
+}
