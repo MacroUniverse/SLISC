b1dd4a82b9f1ce8bbfd3e85dae9c5ae3346793b8
MacroUniverse <270174408@qq.com>
2021-06-28 01:34:41 -0500

    update

--- SLISC/lanczos.h
+++ SLISC/lanczos.h
@@ -521,6 +521,132 @@ inline Doub exp_Hdt_v_lanc(DvecComp_O y, CbandDoub_I H, DvecComp_I x, Doub_I dt,
     return err;
 }
 
+inline Doub exp_Hdt_v_lanc(DvecComp_O y, McooDoub_I H, DvecComp_I x, Doub_I dt, Long_I Nkrylov, VecDoub_IO wsp_d, VecComp_IO wsp_c)
+{
+    Long N = x.size(), Nk = Nkrylov;
+#ifdef SLS_CHECK_SHAPES
+    if (Nk > N)
+        SLS_ERR("Nk > N !");
+    if (wsp_d.size() < Nk*Nk + 2*Nk)
+        SLS_ERR("wsp_d too short!");
+    if (wsp_c.size() < N*Nk + N)
+        SLS_ERR("wsp_c too short!");
+#endif
+
+    Doub *pd = wsp_d.p();
+    SvecDoub alpha(pd, Nk); pd += alpha.size(); // alpha[i] = <v_i|H|v_i>
+    SvecDoub beta(pd, Nk); pd += beta.size(); // beta[i] = norm(bar v_i)
+    ScmatDoub eigV(pd, Nk, Nk); // eigen vectors
+    
+    Comp *pc = wsp_c.p();
+    SvecComp vc(pc, N); pc += vc.size(); // temp vec
+    ScmatComp bases(pc, N, Nk); // Krylov bases
+    
+    SvecComp v0 = cut0(bases, 0), v1 = cut0(bases, 1), v2 = cut0(bases, 2);
+    beta[0] = norm(x);
+    times(v0, x, 1/beta[0]); 
+
+    mul(vc, H, v0);
+    alpha[0] = real(dot(v0, vc));
+    for (Long i = 0; i < N; ++i)
+        v1[i] = vc[i] - alpha[0] * v0[i];
+    beta[1] = norm(v1);
+    v1 /= beta[1];
+    mul(vc, H, v1);
+    alpha[1] = real(dot(v1, vc));
+
+    for (Long j = 0; j < Nk-2; ++j) {
+        Long j1 = j+1, j2 = j+2;
+        mul(v2, H, v1);
+        Doub a = alpha[j1], b = beta[j1];
+        for (Long i = 0; i < N; ++i)
+            v2[i] -= a * v1[i] + b * v0[i];
+        beta[j2] = norm(v2);
+        v2 /= beta[j2];
+        mul(vc, H, v2);
+        alpha[j2] = real(dot(v2, vc));
+        v0.next(); v1.next(); v2.next();
+    }
+    Doub err = prod(beta);
+
+    lapack_int info =
+    LAPACKE_dstev(LAPACK_COL_MAJOR, 'V', Nk, alpha.p(), beta.p()+1, eigV.p(), Nk);
+    // alpha is now eigen values
+    if (info != 0)
+        SLS_ERR("something wrong with LAPACKE_dstev: return = " + num2str(info));
+    
+    for (Long i = 0; i < Nk; ++i)
+        alpha[i] = exp(alpha[i] * dt) * eigV[i*Nk] * beta[0];
+    // alpha, beta are just work spaces now
+    mul_gen(beta, eigV, alpha);
+    mul(y, bases, beta);
+
+    return err;
+}
+
+inline Doub exp_Hdt_v_lanc(DvecComp_O y, McooDoub_I H, DvecComp_I x, Doub_I dt, Long_I Nkrylov, SvecDoub_IO wsp_d, SvecComp_IO wsp_c)
+{
+    Long N = x.size(), Nk = Nkrylov;
+#ifdef SLS_CHECK_SHAPES
+    if (Nk > N)
+        SLS_ERR("Nk > N !");
+    if (wsp_d.size() < Nk*Nk + 2*Nk)
+        SLS_ERR("wsp_d too short!");
+    if (wsp_c.size() < N*Nk + N)
+        SLS_ERR("wsp_c too short!");
+#endif
+
+    Doub *pd = wsp_d.p();
+    SvecDoub alpha(pd, Nk); pd += alpha.size(); // alpha[i] = <v_i|H|v_i>
+    SvecDoub beta(pd, Nk); pd += beta.size(); // beta[i] = norm(bar v_i)
+    ScmatDoub eigV(pd, Nk, Nk); // eigen vectors
+    
+    Comp *pc = wsp_c.p();
+    SvecComp vc(pc, N); pc += vc.size(); // temp vec
+    ScmatComp bases(pc, N, Nk); // Krylov bases
+    
+    SvecComp v0 = cut0(bases, 0), v1 = cut0(bases, 1), v2 = cut0(bases, 2);
+    beta[0] = norm(x);
+    times(v0, x, 1/beta[0]); 
+
+    mul(vc, H, v0);
+    alpha[0] = real(dot(v0, vc));
+    for (Long i = 0; i < N; ++i)
+        v1[i] = vc[i] - alpha[0] * v0[i];
+    beta[1] = norm(v1);
+    v1 /= beta[1];
+    mul(vc, H, v1);
+    alpha[1] = real(dot(v1, vc));
+
+    for (Long j = 0; j < Nk-2; ++j) {
+        Long j1 = j+1, j2 = j+2;
+        mul(v2, H, v1);
+        Doub a = alpha[j1], b = beta[j1];
+        for (Long i = 0; i < N; ++i)
+            v2[i] -= a * v1[i] + b * v0[i];
+        beta[j2] = norm(v2);
+        v2 /= beta[j2];
+        mul(vc, H, v2);
+        alpha[j2] = real(dot(v2, vc));
+        v0.next(); v1.next(); v2.next();
+    }
+    Doub err = prod(beta);
+
+    lapack_int info =
+    LAPACKE_dstev(LAPACK_COL_MAJOR, 'V', Nk, alpha.p(), beta.p()+1, eigV.p(), Nk);
+    // alpha is now eigen values
+    if (info != 0)
+        SLS_ERR("something wrong with LAPACKE_dstev: return = " + num2str(info));
+    
+    for (Long i = 0; i < Nk; ++i)
+        alpha[i] = exp(alpha[i] * dt) * eigV[i*Nk] * beta[0];
+    // alpha, beta are just work spaces now
+    mul_gen(beta, eigV, alpha);
+    mul(y, bases, beta);
+
+    return err;
+}
+
 
 // calculate y = exp(-i*H*dt) * x
 // x, y can be the same object
@@ -968,6 +1094,132 @@ inline Doub exp_miHdt_v_lanc(DvecComp_IO y, CbandDoub_I H, DvecComp_IO x, Doub_I
 }
 
 inline Doub exp_miHdt_v_lanc(DvecComp_IO y, CbandDoub_I H, DvecComp_IO x, Doub_I dt, Long_I Nkrylov, SvecDoub_IO wsp_d, SvecComp_IO wsp_c)
+{
+    Long N = x.size(), Nk = Nkrylov;
+#ifdef SLS_CHECK_SHAPES
+    if (Nk > N)
+        SLS_ERR("Nk > N !");
+    if (wsp_d.size() < Nk*Nk + 2*Nk)
+        SLS_ERR("wsp_d too short!");
+    if (wsp_c.size() < N*Nk + N + Nk)
+        SLS_ERR("wsp_c too short!");
+#endif
+
+    Doub *pd = wsp_d.p();
+    SvecDoub alpha(pd, Nk); pd += alpha.size(); // alpha[i] = <v_i|H|v_i>
+    SvecDoub beta(pd, Nk); pd += beta.size(); // beta[i] = norm(bar v_i)
+    ScmatDoub eigV(pd, Nk, Nk); // eigen vectors
+    
+    Comp *pc = wsp_c.p();
+    SvecComp vc(pc, N); pc += vc.size(); // temp vec
+    SvecComp vc1(pc, Nk); pc += vc1.size(); // temp vec
+    ScmatComp bases(pc, N, Nk); // Krylov bases
+    
+    SvecComp v0 = cut0(bases, 0), v1 = cut0(bases, 1), v2 = cut0(bases, 2);
+    beta[0] = norm(x);
+    times(v0, x, 1/beta[0]);
+    mul(vc, H, v0);
+    alpha[0] = real(dot(v0, vc));
+    for (Long i = 0; i < N; ++i)
+        v1[i] = vc[i] - alpha[0] * v0[i];
+    beta[1] = norm(v1);
+    v1 /= beta[1];
+    mul(vc, H, v1);
+    alpha[1] = real(dot(v1, vc));
+
+    for (Long j = 0; j < Nk-2; ++j) {
+        Long j1 = j+1, j2 = j+2;
+        mul(v2, H, v1);
+        Doub a = alpha[j1], b = beta[j1];
+        for (Long i = 0; i < N; ++i)
+            v2[i] -= a * v1[i] + b * v0[i];
+        beta[j2] = norm(v2);
+        v2 /= beta[j2];
+        mul(vc, H, v2);
+        alpha[j2] = real(dot(v2, vc));
+        v0.next(); v1.next(); v2.next();
+    }
+    Doub err = prod(beta);
+
+    lapack_int info =
+    LAPACKE_dstev(LAPACK_COL_MAJOR, 'V', Nk, alpha.p(), beta.p()+1, eigV.p(), Nk);
+    // alpha is now eigen values
+    if (info != 0)
+        SLS_ERR("something wrong with LAPACKE_dstev: return = " + num2str(info));
+    
+    SvecComp vc2 = cut(vc, 0, Nk);
+    for (Long i = 0; i < Nk; ++i)
+        vc2[i] = exp(Comp(0,-1) * (alpha[i] * dt)) * conj(eigV[i*Nk]) * beta[0];
+    mul_gen(vc1, eigV, vc2);
+    mul_gen(y, bases, vc1);
+
+    return err;
+}
+
+inline Doub exp_miHdt_v_lanc(DvecComp_IO y, McooDoub_I H, DvecComp_IO x, Doub_I dt, Long_I Nkrylov, VecDoub_IO wsp_d, VecComp_IO wsp_c)
+{
+    Long N = x.size(), Nk = Nkrylov;
+#ifdef SLS_CHECK_SHAPES
+    if (Nk > N)
+        SLS_ERR("Nk > N !");
+    if (wsp_d.size() < Nk*Nk + 2*Nk)
+        SLS_ERR("wsp_d too short!");
+    if (wsp_c.size() < N*Nk + N + Nk)
+        SLS_ERR("wsp_c too short!");
+#endif
+
+    Doub *pd = wsp_d.p();
+    SvecDoub alpha(pd, Nk); pd += alpha.size(); // alpha[i] = <v_i|H|v_i>
+    SvecDoub beta(pd, Nk); pd += beta.size(); // beta[i] = norm(bar v_i)
+    ScmatDoub eigV(pd, Nk, Nk); // eigen vectors
+    
+    Comp *pc = wsp_c.p();
+    SvecComp vc(pc, N); pc += vc.size(); // temp vec
+    SvecComp vc1(pc, Nk); pc += vc1.size(); // temp vec
+    ScmatComp bases(pc, N, Nk); // Krylov bases
+    
+    SvecComp v0 = cut0(bases, 0), v1 = cut0(bases, 1), v2 = cut0(bases, 2);
+    beta[0] = norm(x);
+    times(v0, x, 1/beta[0]);
+    mul(vc, H, v0);
+    alpha[0] = real(dot(v0, vc));
+    for (Long i = 0; i < N; ++i)
+        v1[i] = vc[i] - alpha[0] * v0[i];
+    beta[1] = norm(v1);
+    v1 /= beta[1];
+    mul(vc, H, v1);
+    alpha[1] = real(dot(v1, vc));
+
+    for (Long j = 0; j < Nk-2; ++j) {
+        Long j1 = j+1, j2 = j+2;
+        mul(v2, H, v1);
+        Doub a = alpha[j1], b = beta[j1];
+        for (Long i = 0; i < N; ++i)
+            v2[i] -= a * v1[i] + b * v0[i];
+        beta[j2] = norm(v2);
+        v2 /= beta[j2];
+        mul(vc, H, v2);
+        alpha[j2] = real(dot(v2, vc));
+        v0.next(); v1.next(); v2.next();
+    }
+    Doub err = prod(beta);
+
+    lapack_int info =
+    LAPACKE_dstev(LAPACK_COL_MAJOR, 'V', Nk, alpha.p(), beta.p()+1, eigV.p(), Nk);
+    // alpha is now eigen values
+    if (info != 0)
+        SLS_ERR("something wrong with LAPACKE_dstev: return = " + num2str(info));
+    
+    SvecComp vc2 = cut(vc, 0, Nk);
+    for (Long i = 0; i < Nk; ++i)
+        vc2[i] = exp(Comp(0,-1) * (alpha[i] * dt)) * conj(eigV[i*Nk]) * beta[0];
+    mul_gen(vc1, eigV, vc2);
+    mul_gen(y, bases, vc1);
+
+    return err;
+}
+
+inline Doub exp_miHdt_v_lanc(DvecComp_IO y, McooDoub_I H, DvecComp_IO x, Doub_I dt, Long_I Nkrylov, SvecDoub_IO wsp_d, SvecComp_IO wsp_c)
 {
     Long N = x.size(), Nk = Nkrylov;
 #ifdef SLS_CHECK_SHAPES
