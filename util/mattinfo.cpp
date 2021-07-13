abed49d1bdded3cf1c4a2a8f36f27ec7e50bd649
MacroUniverse <270174408@qq.com>
2021-07-13 15:35:45 -0500

    add mattinfo

---
+++ util/mattinfo.cpp
@@ -0,0 +1,26 @@
+#include "../SLISC/matt.h"
+
+using namespace slisc;
+
+int main(int argc, char **argv)
+{
+	Str fname = argv[1];
+	Matt matt(fname, "r");
+	for (Long i = 0; i < matt.size(); ++i) {
+		cout << i << ". " << matt.m_name[i] << "  size: ";
+		if (size(matt.m_size[i]) > 0) {
+			cout << matt.m_size[i][0];
+			for (Long j = 1; j < size(matt.m_size[i]); ++j) {
+				cout << "x" << matt.m_size[i][j];
+			}
+		}
+		else
+			cout << '1';
+		
+		cout << "  type: " << matt.m_type[i] << endl;
+//% types = {};
+//%----------------------------------
+// TODO
+//%----------------------------------
+	}
+}
