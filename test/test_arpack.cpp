cfc2848de801b1dee75e7832839fa444ae307cf4
MacroUniverse <270174408@qq.com>
2022-01-06 15:09:42 -0600

    Update test_arpack.cpp

--- test/test_arpack.cpp
+++ test/test_arpack.cpp
@@ -7,28 +7,53 @@ using namespace slisc;
 class MyMatrix {
 
 private:
-		CmatDoub a;
+	Long m_N;
+	Long m_count;
+
 public:
-	MyMatrix(Long N) {
-		a.resize(N, N); copy(a, 0);
-		for (Long i = 0; i < N-1; ++i) {
-			a(i, i+1) = a(i+1, i) = 1;
-		}
-	}
+	MyMatrix(Long N): m_N(N), m_count(0) {}
 
 	void mul(Doub *v, Doub* w) {
-		SvecDoub sv(v, a.n1());
-		SvecDoub sw(w, a.n0());
-		slisc::mul(sw, a, sv);
+		// matrix-vector mul with matrix A: subdiagonal = 1, other = 0
+		for (Long i = 0; i < m_N-1; ++i)
+			w[i] = v[i+1];
+		w[m_N-1] = 0;
+		for (Long i = 0; i < m_N-1; ++i)
+			w[i+1] += v[i];
+
+		// report
+		++m_count;
+		if (m_count % 100 == 0) {
+            // cout << "mul count: " << m_count << ", err = " << err(v, w) << endl;
+        }
+	}
+
+	// mean value / eigen value
+    Doub mean(const Doub *v, const Doub *w) const
+    {
+        SvecDoub_c sv(v, m_N), sw(w, m_N);
+        return dot(sv, sw)/dot(sv, sv);
+    }
+
+	Doub err(const Doub *v, const Doub *w) const
+	{
+		Doub val = mean(v, w);
+		Doub nrm2 = 0;
+		for (Long i = 0; i < m_N; ++i)
+			nrm2 += sqr(abs(w[i] - val * v[i]));
+		return sqrt(nrm2);
 	}
 };
 
 void test_arpack()
 {
-	Long N = 5;
+	Long N = 10, Nsol = 3;
 	MyMatrix a(N);
-  	ARSymStdEig<Doub, MyMatrix> dprob(N, 3, &a, &MyMatrix::mul, "SM");
+  	ARSymStdEig<Doub, MyMatrix> dprob(N, Nsol, &a, &MyMatrix::mul, "SM");
+	dprob.ChangeMaxit(1000000);
+	dprob.ChangeTol(1e-2); // ||Av - lambda v|| is usually 10-100 times smaller thant Tol
 	dprob.FindEigenvectors();
+	// cout << "\n\n============ done ===============\n\n" << endl;
 
 	// print solution:
 	// Long n = dprob.GetN(); // # dim
@@ -44,23 +69,15 @@ void test_arpack()
 	for (Long i = 0; i < Nconv; ++i)
 		eval[i] = dprob.Eigenvalue(i);
 
-	if (abs(eval[0] + 1) > 1e-10)
-		SLS_ERR("failed!");
-	if (abs(eval[1]) > 1e-10)
-		SLS_ERR("failed!");
-	if (abs(eval[2] - 1) > 1e-10)
-		SLS_ERR("failed!");
-
 	VecDoub av(N);
 	for (Long j = 0; j < Nconv; ++j) {
 		SvecDoub v(dprob.RawEigenvector(j), N);
 		if (abs(norm(v) - 1) > 1e-10)
 			SLS_ERR("failed!");
 		a.mul(v.p(), av.p());
-		for (Long i = 0; i < N; ++i)
-			av[i] -= eval[j] * v[i];
-		// cout << j << ": " << abs(norm(av)) << endl;
-		if (abs(norm(av)) > 1e-10)
+		Doub err = a.err(v.p(), av.p());
+		// cout << "err = " << err << endl;
+		if (err > 1e-2)
 			SLS_ERR("failed!");
 	}
 }
