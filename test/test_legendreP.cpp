7b2b154967494123f49610dee9a894dc291f8354
MacroUniverse <270174408@qq.com>
2022-05-24 23:04:13 -0500

    test legendre polynomial
    * for quad math

---
+++ test/test_legendreP.cpp
@@ -0,0 +1,8 @@
+#include "../SLISC/legendreP.h"
+
+void test_legendreP()
+{
+    using namespace slisc;
+	if (abs(legendre_Plm(20, 4, 0.7Q) + 3.5920748264608943176145619300842e4Q) > 1e-26)
+		SLS_ERR("failed!");
+}
