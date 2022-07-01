5f0429d0dd15d86fe476be0a970e0531966ca15e
MacroUniverse <270174408@qq.com>
2022-07-01 01:02:33 -0500

    test Ldoub

--- test/test_global.cpp
+++ test/test_global.cpp
@@ -18,12 +18,19 @@ void test_global()
 	if (sizeof(Ullong) != 8) SLS_ERR("failed!");
 	if (sizeof(Float) != 4) SLS_ERR("failed!");
 	if (sizeof(Doub) != 8) SLS_ERR("failed!");
-	if (sizeof(Ldoub) != 10) SLS_WARN("Ldoub bytes: " + to_string(sizeof(Ldoub)));
 	if (sizeof(Fcomp) != 8) SLS_ERR("failed!");
 	if (sizeof(Comp) != 16) SLS_ERR("failed!");
-	if (sizeof(Lcomp) != 20) SLS_WARN("Lcomp bytes: " + to_string(sizeof(Lcomp)));
 #ifdef SLS_USE_QUAD
 	if (sizeof(Qdoub) != 16) SLS_ERR("failed!");
 	if (sizeof(Qcomp) != 32) SLS_ERR("failed!");
 #endif
+	if (sizeof(Ldoub) != 10) {
+		SLS_WARN("Ldoub bytes: " + to_string(sizeof(Ldoub)));
+		SLS_WARN("Lcomp bytes: " + to_string(sizeof(Lcomp)));
+	}
+	//% check if is 80 bit implementation
+	if (abs(std::numeric_limits<Ldoub>::max() - 1.1897314e+4932L) > 1e4926L)
+		SLS_WARN("Ldoub max =" + to_string(std::numeric_limits<Ldoub>::max()));
+	if (log2(std::numeric_limits<Ldoub>::epsilon()) != -63)
+		SLS_WARN("Ldoub mantessa bits: =" + to_string(log2(std::numeric_limits<Ldoub>::epsilon())));
 }
