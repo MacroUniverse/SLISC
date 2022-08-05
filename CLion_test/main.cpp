ac13bebcfdd6c0071873c3d23892dd6008ce71e6
MacroUniverse <270174408@qq.com>
2022-08-05 06:09:21 -0500

    Update main.cpp

--- CLion_test/main.cpp
+++ CLion_test/main.cpp
@@ -1,19 +1,19 @@
+// Build time: [iostream] 4.2s [STL] 5.2s [SLISC] 8.5s
 #include <bits/stdc++.h>
 #include "../SLISC/STL_util.h"
-#include "../SLISC/arithmetic.h"
-#include "../SLISC/sort.h"
 #include "../SLISC/search.h"
+#include "../SLISC/sort.h"
 #include "../SLISC/random.h"
-#include "../SLISC/string.h"
-#include "../SLISC/Bit.h"
-#include "../SLISC/disp.h"
-#include "../SLISC/file.h"
-#include "../SLISC/input.h"
 #include "../SLISC/sing_list.h"
 #include "../SLISC/bin_tree.h"
 #include "../SLISC/DAG.h"
-
+#include "../SLISC/string.h"
+#include "../SLISC/Bit.h"
+#include "../SLISC/disp.h"
+#include "../SLISC/input.h"
+#include "../SLISC/file.h"
 using namespace slisc;
+
 using namespace std;
 
 inline void read_input(vector<int> &v_num, vector<string> &v_str) {
@@ -21,17 +21,14 @@ inline void read_input(vector<int> &v_num, vector<string> &v_str) {
     while (cin >> num >> str) {
         v_num.push_back(num);
         v_str.push_back(move(str));
-        cin_ignore_line();
+        cin.ignore(ULONG_LONG_MAX, '\n');
     }
 }
 
 int main() {
     vector<int> v_num, v_num1; vector<string> v_str;
     read_input(v_num, v_str);
-    //----------------------------------------------
-    vector<DAGnode> dag;
-    dag_examp1(dag);
-    vector<Long> path;
-    dag_shortest_path(path, dag, 0, 9);
+//----------------------------------------------
+    cout << "hello!" << endl;
     return 0;
 }
