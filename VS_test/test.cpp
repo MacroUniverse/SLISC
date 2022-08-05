2d87877b018133f4c6d55d1f51e508b03337d77d
MacroUniverse <270174408@qq.com>
2022-08-04 21:33:04 -0500

    VS_test and CLion_test both working

--- VS_test/test.cpp
+++ VS_test/test.cpp
@@ -1,20 +1,66 @@
-// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "../SLISC/STL_util.h"
+#include "../SLISC/arithmetic.h"
+#include "../SLISC/sort.h"
+#include "../SLISC/search.h"
+#include "../SLISC/random.h"
+#include "../SLISC/string.h"
+#include "../SLISC/Bit.h"
+#include "../SLISC/disp.h"
+#include "../SLISC/file.h"
+#include "../SLISC/input.h"
+#include "../SLISC/sing_list.h"
+#include "../SLISC/bin_tree.h"
+#include "../SLISC/DAG.h"
 
-#include <iostream>
+using namespace slisc;
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+inline void read_input(vector<int>& v_num, vector<string>& v_str) {
+    int num; string str;
+    while (cin >> num >> str) {
+        v_num.push_back(num);
+        v_str.push_back(move(str));
+        cin_ignore_line();
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+    int N = 111;
+    unordered_map<int, char> visited; // (node, last_mv)
+    vector<int> nodes{ N }, nodes1;
+    visited[N] = 's';
+    while (!nodes.empty()) {
+        for (auto& node : nodes) {
+            // jump
+            if (node % 2 == 0 && node > 2 && !visited.count(node / 2)) {
+                nodes1.push_back(node / 2);
+                visited[node / 2] = 'j';
+            }
+            // down
+            if (!visited.count(node - 1)) {
+                nodes1.push_back(node - 1);
+                visited[node - 1] = 'd';
+                if (node == 1)
+                    goto break2;
+            }
+            // up
+            if (!visited.count(node + 1)) {
+                nodes1.push_back(node + 1);
+                visited[node + 1] = 'u';
+            }
+        }
+        swap(nodes, nodes1); nodes1.clear();
+    }
+break2:
+    nodes.clear(); // fastest path
+    nodes.push_back(0);
+    while (nodes.back() != N) {
+        if (visited[nodes.back()] == 'u')
+            nodes.push_back(nodes.back() - 1);
+        else if (visited[nodes.back()] == 'd')
+            nodes.push_back(nodes.back() + 1);
+        else if (visited[nodes.back()] == 'j')
+            nodes.push_back(nodes.back() * 2);
+    }
+    return 10;
+}
