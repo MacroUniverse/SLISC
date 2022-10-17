09fe4008cc4f100f7004391b4b3dc548324fdd87
MacroUniverse <270174408@qq.com>
2022-10-17 16:09:02 -0500

    minor fix for ubuntu 20.04

--- SLISC/arb_extension.h
+++ SLISC/arb_extension.h
@@ -8,7 +8,8 @@
 
 namespace slisc {
 
-#if __ARB_VERSION == 2 && __ARB_VERSION_MINOR <= 21
+// __ARB_VERSION might not be defined in older versions
+#if !defined(__ARB_VERSION) || (__ARB_VERSION == 2 && __ARB_VERSION_MINOR <= 21)
 // similar to arb_get_str()
 inline char * arf_get_str(const arf_t x, slong prec)
 {
