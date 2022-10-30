56256d0b480286cb4fbc8ff1828834374d4d648b
MacroUniverse <270174408@qq.com>
2022-10-29 20:56:17 -0500

    Update interv.h

--- SLISC/interv.h
+++ SLISC/interv.h
@@ -16,234 +16,251 @@ typedef Intvs &Intvs_O, &Intvs_IO;
 class Intvs : public vecLong
 {
 public:
-	typedef vecLong Base;
-	void push(Long_I left, Long_I right);
-	void pushL(Long_I i);
-	void pushR(Long_I i);
-	void push_back(Long_I i);
-	void check_pair() const;
-	Long size() const;
-	const Long &L(Long_I i) const;
-	Long &L(Long_I i);
-	const Long &R(Long_I i) const;
-	Long &R(Long_I i);
-	const Long &operator[](Long_I i) const;
-	Long &operator[](Long_I i);
-	void erase(Long_I start, Long_I count);
+    typedef vecLong Base;
+    void push(Long_I left, Long_I right);
+    void pushL(Long_I i);
+    void pushR(Long_I i);
+    void push_back(Long_I i);
+    void check_pair() const;
+    Long size() const;
+    const Long &L(Long_I i) const;
+    Long &L(Long_I i);
+    const Long &R(Long_I i) const;
+    Long &R(Long_I i);
+    const Long &operator[](Long_I i) const;
+    Long &operator[](Long_I i);
+    void erase(Long_I start, Long_I count = 1);
 };
 
 inline void Intvs::push(Long_I left, Long_I right)
 {
 #ifdef SLS_CHECK_BOUNDS
-	if (left < 0 || right < 0)
-	    SLS_ERR("must be non-negative!");
-	if (isodd((Long)Base::size()))
-	    SLS_ERR("last pair not finished!");
+    if (left < 0 || right < 0)
+        SLS_ERR("must be non-negative!");
+    if (isodd((Long)Base::size()))
+        SLS_ERR("last pair not finished!");
 #endif
-	push_back(left);
-	push_back(right);
+    push_back(left);
+    push_back(right);
 }
 
 inline void Intvs::pushL(Long_I i)
 {
 #ifdef SLS_CHECK_BOUNDS
-	if (i < 0)
-	    SLS_ERR("must be non-negative!");
-	if (isodd((Long)Base::size()))
-	    SLS_ERR("last pair not finished!");
+    if (i < 0)
+        SLS_ERR("must be non-negative!");
+    if (isodd((Long)Base::size()))
+        SLS_ERR("last pair not finished!");
 #endif
-	push_back(i);
+    push_back(i);
 }
 
 inline void Intvs::pushR(Long_I i)
 {
 #ifdef SLS_CHECK_BOUNDS
-	if (i < 0)
-	    SLS_ERR("must be non-negative!");
-	if (!isodd((Long)Base::size()))
-	    SLS_ERR("left not pushed!");
+    if (i < 0)
+        SLS_ERR("must be non-negative!");
+    if (!isodd((Long)Base::size()))
+        SLS_ERR("left not pushed!");
 #endif
-	push_back(i);
+    push_back(i);
 }
 
 inline void Intvs::push_back(Long_I i)
 {
-	Base::push_back(i);
+    Base::push_back(i);
 }
 
 inline void Intvs::check_pair() const
 {
-	if (isodd((Long)Base::size()))
-	    SLS_ERR("side is odd!");
+    if (isodd((Long)Base::size()))
+        SLS_ERR("side is odd!");
 }
 
 inline Long Intvs::size() const
 {
-	if (isodd((Long)Base::size()))
-	    SLS_ERR("last pair unfinished!");
-	return Base::size() / 2;
+    if (isodd((Long)Base::size()))
+        SLS_ERR("last pair unfinished!");
+    return Base::size() / 2;
 }
 
 inline const Long &Intvs::L(Long_I i) const
 {
 #ifdef SLS_CHECK_BOUNDS
-	if (i < 0 || i * 2 >= (Long)Base::size())
-	    SLS_ERR("out of bound!");
+    if (i < 0 || i * 2 >= (Long)Base::size())
+        SLS_ERR("out of bound!");
 #endif
-	return Base::operator[](i * 2);
+    return Base::operator[](i * 2);
 }
 
 inline Long &Intvs::L(Long_I i)
 {
 #ifdef SLS_CHECK_BOUNDS
-	if (i < 0 || i * 2 >= (Long)Base::size())
-	    SLS_ERR("out of bound!");
+    if (i < 0 || i * 2 >= (Long)Base::size())
+        SLS_ERR("out of bound!");
 #endif
-	return Base::operator[](i * 2);
+    return Base::operator[](i * 2);
 }
 
 inline const Long &Intvs::R(Long_I i) const
 {
 #ifdef SLS_CHECK_BOUNDS
-	if (i < 0 || i * 2 + 1 >= (Long)Base::size())
-	    SLS_ERR("out of bound!");
+    if (i < 0 || i * 2 + 1 >= (Long)Base::size())
+        SLS_ERR("out of bound!");
 #endif
-	return Base::operator[](i * 2 + 1);
+    return Base::operator[](i * 2 + 1);
 }
 
 inline Long &Intvs::R(Long_I i)
 {
 #ifdef SLS_CHECK_BOUNDS
-	if (i < 0 || i * 2 + 1 >= (Long)Base::size())
-	    SLS_ERR("out of bound!");
+    if (i < 0 || i * 2 + 1 >= (Long)Base::size())
+        SLS_ERR("out of bound!");
 #endif
-	return Base::operator[](i * 2 + 1);
+    return Base::operator[](i * 2 + 1);
 }
 
 inline const Long &Intvs::operator[](Long_I i) const
 {
-	SLS_ERR("use .L() or .R() instead!");
-	return Base::operator[](i);
+    SLS_ERR("use .L() or .R() instead!");
+    return Base::operator[](i);
 }
 
 inline Long &Intvs::operator[](Long_I i)
 {
-	SLS_ERR("use .L() or .R() instead!");
-	return Base::operator[](i);
+    SLS_ERR("use .L() or .R() instead!");
+    return Base::operator[](i);
 }
 
 inline void Intvs::erase(Long_I start, Long_I count)
 {
-	auto temp = Base::begin() + 2 * start;
-	Base::erase(temp, temp + 2 * count);
+    auto temp = Base::begin() + 2 * start;
+    Base::erase(temp, temp + 2 * count);
 }
 
-// see if an index i falls into the scopes of ind
-inline Bool is_in(Long_I i, Intvs_I intvs)
+// see if an index i falls into the scopes of intv
+inline Bool is_in(Long_I i, Intvs_I intv)
 {
-	for (Long j = 0; j < intvs.size(); ++j) {
-	    if (intvs.L(j) <= i && i <= intvs.R(j))
-	        return true;
-	}
-	return false;
+    for (Long j = 0; j < intv.size(); ++j) {
+        if (intv.L(j) <= i && i <= intv.R(j))
+            return true;
+    }
+    return false;
 }
 
 // if is_in() is true, return the number of interval
 // if not true, return -1
-inline Long is_in_no(Long_I i, Intvs_I intvs)
+inline Long is_in_no(Long_I i, Intvs_I intv)
 {
-	for (Long j = 0; j < intvs.size(); ++j) {
-	    if (intvs.L(j) <= i && i <= intvs.R(j))
-	        return j;
-	}
-	return -1;
+    for (Long j = 0; j < intv.size(); ++j) {
+        if (intv.L(j) <= i && i <= intv.R(j))
+            return j;
+    }
+    return -1;
 }
 
-// invert ranges in ind0, output to ind1
+// invert ranges in intv0, output to intv1
 // [0, N-1] is the total domain
-inline Long invert(Intvs_O ind, Intvs_I ind0, Long_I N)
+Long invert(Intvs_O intv, Intvs_I intv0, Long_I N)
 {
-	ind.clear();
-	if (ind0.size() == 0) {
-	    ind.push(0, N - 1);
-	    return 1;
-	}
+    intv.clear();
+    if (intv0.size() == 0) {
+        intv.push(0, N - 1);
+        return 1;
+    }
 
-	Long count{}; // total num of ranges output
-	if (ind0.L(0) > 0) {
-	    ind.push(0, ind0.L(0) - 1); ++count;
-	}
-	for (Long i = 0; i < ind0.size()-1; ++i) {
-	    ind.push(ind0.R(i) + 1, ind0.L(i+1) - 1);
-	    ++count;
-	}
-	if (ind0.back() < N - 1) {
-	    ind.push(ind0.back() + 1, N - 1); ++count;
-	}
-	return count;
+    Long count{}; // total num of ranges output
+    if (intv0.L(0) > 0) {
+        intv.push(0, intv0.L(0) - 1); ++count;
+    }
+    for (Long i = 0; i < intv0.size()-1; ++i) {
+        intv.push(intv0.R(i) + 1, intv0.L(i+1) - 1);
+        ++count;
+    }
+    if (intv0.back() < N - 1) {
+        intv.push(intv0.back() + 1, N - 1); ++count;
+    }
+    return count;
 }
 
-// combine ranges ind1 and ind2
+// combine ranges intv1 and intv2
 // a range can contain another range, but not partial overlap
 // return total range number
-inline Long combine(Intvs_O ind, Intvs_I ind1, Intvs_I ind2)
+inline Long combine(Intvs_O intv, Intvs_I intv1, Intvs_I intv2)
 {
-	Long i, N1 = ind1.size(), N2 = ind2.size();
-	ind1.check_pair(); ind2.check_pair();
-	if (&ind == &ind1 || &ind == &ind2) {
-	    SLS_ERR("aliasing is not allowed!");
-	}
-	if (N1 == 0) {
-	    ind = ind2; return N2;
-	}
-	else if (N2 == 0) {
-	    ind = ind1; return N1;
-	}
+    Long i, N1 = intv1.size(), N2 = intv2.size();
+    intv1.check_pair(); intv2.check_pair();
+    if (&intv == &intv1 || &intv == &intv2) {
+        SLS_ERR("aliasing is not allowed!");
+    }
+    if (N1 == 0) {
+        intv = intv2; return N2;
+    }
+    else if (N2 == 0) {
+        intv = intv1; return N1;
+    }
 
-	// load start and end, and sort
-	vecLong start, end;
-	for (i = 0; i < N1; ++i)
-	    start.push_back(ind1.L(i));
-	for (i = 0; i < N1; ++i)
-	    end.push_back(ind1.R(i));
-	for (i = 0; i < N2; ++i)
-	    start.push_back(ind2.L(i));
-	for (i = 0; i < N2; ++i)
-	    end.push_back(ind2.R(i));
-	sort(start, end);
+    // load start and end, and sort
+    vecLong start, end;
+    for (i = 0; i < N1; ++i)
+        start.push_back(intv1.L(i));
+    for (i = 0; i < N1; ++i)
+        end.push_back(intv1.R(i));
+    for (i = 0; i < N2; ++i)
+        start.push_back(intv2.L(i));
+    for (i = 0; i < N2; ++i)
+        end.push_back(intv2.R(i));
+    sort(start, end);
 
-	// load ind
-	ind.clear();
-	ind.pushL(start[0]);
-	i = 0;
-	while (i < size(start) - 1) {
-	    if (end[i] > start[i + 1]) {
-	        if (end[i] > end[i + 1]) {
-	            end[i + 1] = end[i]; ++i;
-	        }
-	        else {
-	            throw Str32(U"error! range overlap!");
-	        }
-	    }
-	    else if (end[i] == start[i + 1] - 1)
-	        ++i;
-	    else {
-	        ind.pushR(end[i]);
-	        ind.pushL(start[i + 1]);
-	        ++i;
-	    }
-	}
-	ind.pushR(end.back());
-	return ind.size();
+    // load intv
+    intv.clear();
+    intv.pushL(start[0]);
+    i = 0;
+    while (i < size(start) - 1) {
+        if (end[i] > start[i + 1]) {
+            if (end[i] > end[i + 1]) {
+                end[i + 1] = end[i]; ++i;
+            }
+            else {
+                throw Str32(U"error! range overlap!");
+            }
+        }
+        else if (end[i] == start[i + 1] - 1)
+            ++i;
+        else {
+            intv.pushR(end[i]);
+            intv.pushL(start[i + 1]);
+            ++i;
+        }
+    }
+    intv.pushR(end.back());
+    return intv.size();
 }
 
-// combine intervals ind and ind1, and assign the result to ind
-inline Long combine(Intvs_IO ind, Intvs_I ind1)
+// combine intervals intv and intv1, and assign the result to intv
+inline Long combine(Intvs_IO intv, Intvs_I intv1)
 {
-	Intvs temp;
-	Long ret = combine(temp, ind, ind1);
-	ind = temp;
-	return ret;
+    Intvs temp;
+    Long ret = combine(temp, intv, intv1);
+    intv = temp;
+    return ret;
+}
+
+// exclude intervals in `intv` that's completely insize `intv1`
+// throw error on partial overlapping
+inline void exclude(Intvs_IO intv, Intvs_I intv1)
+{
+    Long N = intv.size();
+    for (Long i = 0; i < N; ++i) {
+        if (is_in(intv.L(i), intv1)) {
+            if (is_in(intv.R(i), intv1))
+                intv.erase(i), --i; // erase is not very efficient
+            else
+                throw Str32(U"intv[" + num2str32(i) + U"] partial overlap on the left!");
+        }
+        else if (is_in(intv.R(i), intv1))
+            throw Str32(U"intv[" + num2str32(i) + U"] partial overlap on the right!");
+    }
 }
 
 } // namespace slisc
