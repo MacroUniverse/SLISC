a599d659cff65b762d1079d42a3dfb275250a9c0
MacroUniverse <270174408@qq.com>
2023-08-18 02:36:31 -0500

    update scratch.cpp/inp

--- scratch.cpp
+++ scratch.cpp
@@ -9,45 +9,36 @@
 // [GCH:STL+SLISC] 1.7s
 // #include <bits/stdc++.h>
 
-#include "SLISC/util/STL_util.h"
-#include "SLISC/algo/search.h"
-#include "SLISC/algo/heap.h"
-#include "SLISC/algo/sort.h"
-#include "SLISC/algo/sing_list.h"
-#include "SLISC/algo/bin_tree.h"
-#include "SLISC/algo/disjoint_sets.h"
-#include "SLISC/algo/graph.h"
-#include "SLISC/algo/queue.h"
-#include "SLISC/algo/disjoint_sets.h"
-#include "SLISC/algo/hungarian.h"
-#include "SLISC/algo/huffman.h"
-// #define SLS_RAND_SEED 0
-#include "SLISC/util/random.h"
-#include "SLISC/util/bit.h"
+// #include "SLISC/util/STL_util.h"
+// #include "SLISC/algo/search.h"
+// #include "SLISC/algo/heap.h"
+// #include "SLISC/algo/sort.h"
+// #include "SLISC/algo/sing_list.h"
+// #include "SLISC/algo/bin_tree.h"
+// #include "SLISC/algo/disjoint_sets.h"
+// #include "SLISC/algo/graph.h"
+// #include "SLISC/algo/queue.h"
+// #include "SLISC/algo/disjoint_sets.h"
+// #include "SLISC/algo/hungarian.h"
+// #include "SLISC/algo/huffman.h"
+// // #define SLS_RAND_SEED 0
+// #include "SLISC/util/random.h"
+// #include "SLISC/util/bit.h"
 #include "SLISC/util/input.h"
-#include "SLISC/str/str.h"
-#include "SLISC/str/disp.h"
-#include "SLISC/file/file.h"
+// #include "SLISC/str/str.h"
+// #include "SLISC/str/disp.h"
+// #include "SLISC/file/file.h"
 
 using namespace slisc;
+// using namespace std;
 
-using namespace std;
 
-inline void read_input(vector<int> &v_num, vector<string> &v_str) {
-    int num; string str;
-    while (cin >> num >> str) {
-        v_num.push_back(num);
-        v_str.push_back(move(str));
-        cin.ignore(ULLONG_MAX, '\n');
-    }
-}
-
+// usage: `./scratch.x < scratch.inp`
 int main() {
     vector<int> v_num, v_num1; vector<string> v_str;
-    read_input(v_num, v_str);
-    disp(v_num);
-    disp(v_str);
-//----------------------------------------------
-    cout << "done!" << endl;
+    read_input2(v_num, v_str);
+	cout << "\n\nyour input:" << endl;
+	for (size_t i = 0; i < v_num.size(); ++i)
+		cout << v_num[i] << "  " << v_str[i] << endl; 
     return 0;
 }
