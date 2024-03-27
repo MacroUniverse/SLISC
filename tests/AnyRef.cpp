95929ec08f3993fc60a420066202088e156e8cbf
MacroUniverse <2701744082qq.com>
2024-03-27 03:50:08 -0500

    AnyRef use code gen

--- tests/AnyRef.cpp
+++ tests/AnyRef.cpp
@@ -9,9 +9,11 @@ void test_AnyRef()
 	ref.bind(vs[1]);
 	SLS_ASSERT(ref.is<Str>());
 	SLS_ASSERT(ref.get<Str>() == "bcd");
+	SLS_ASSERT(hash_AnyRef{}(ref) == std::hash<Str>{}(vs[1]));
 	ref.bind(vi[2]);
 	SLS_ASSERT(ref.is<Int>());
 	SLS_ASSERT(ref.get<Int>() == 3);
+	SLS_ASSERT(hash_AnyRef{}(ref) == std::hash<Int>{}(vi[2]));
 }
 
 #ifndef SLS_TEST_ALL
