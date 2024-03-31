d45eeed59bcb00b826ea09a615ac4db09a345d5d
MacroUniverse <270174408@qq.com>
2024-03-31 17:30:02 -0500

    sqlitecpp_ext.h.in

--- SLISC/file/sqlitecpp_ext.h
+++ SLISC/file/sqlitecpp_ext.h
@@ -35,15 +35,17 @@ inline void update_sqlite_table_query(
 	q_insert << ");";
 
 	// UPDATE statement
-	clear(q_update) << "UPDATE \"" << table_name << "\" SET ";
-	for (Long i = Npk; i < size(field_names); ++i)
-		q_update << '"' << field_names[i] << "\"=?, ";
-	q_update.resize(q_update.size()-2);
-	q_update << " WHERE ";
-	for (Long i = 0; i < Npk; ++i)
-		q_update << '"' << field_names[i] << "\"=? AND ";
-	q_update.resize(q_update.size()-5);
-	q_update << ';';
+	if (Npk < size(field_names)) {
+		clear(q_update) << "UPDATE \"" << table_name << "\" SET ";
+		for (Long i = Npk; i < size(field_names); ++i)
+			q_update << '"' << field_names[i] << "\"=?, ";
+		q_update.resize(q_update.size()-2);
+		q_update << " WHERE ";
+		for (Long i = 0; i < Npk; ++i)
+			q_update << '"' << field_names[i] << "\"=? AND ";
+		q_update.resize(q_update.size()-5);
+		q_update << ';';
+	}
 
 	// DELETE statement
 	clear(q_delete) << "DELETE FROM \"" << table_name << "\" WHERE ";
@@ -117,5 +119,47 @@ inline void update_sqlite_table(
 	}
 }
 
+// sync `data` to a db table, perform DELETE and UPDATE as needed, unchanged records won't be touched
+// only record in db satisfying `condition` will be checked
+// assume `data` satisfy `condition` (no check)
+// might violate 'UNIQUE' for non-primary-key fields
+inline void update_sqlite_table(
+	unordered_map<tuple<Str,Str>, tuple<>> &data, // (primary keys) -> (other fields)
+	Str_I table_name,
+	Str_I condition, // the SQL statement after "WHERE"
+	vecStr_I field_names, // field names of `data`, in order
+	Long_I Npk, // number of primary keys
+	SQLite::Database &db_rw
+) {
+	Str q_select, q_insert, q_update, q_delete;
+	update_sqlite_table_query(q_select, q_insert, q_update, q_delete,
+		table_name, condition, field_names, Npk);
+	SQLite::Statement stmt_select(db_rw, q_select);
+	SQLite::Statement stmt_insert(db_rw, q_insert);
+	SQLite::Statement stmt_delete(db_rw, q_delete);
+
+	while (stmt_select.executeStep()) {
+		auto key = make_tuple(
+			(Str)stmt_select.getColumn(0).getString(),
+			(Str)stmt_select.getColumn(1).getString()
+		);
+		if (!data.count(key)) {
+			// key not found, deleted
+			stmt_delete.bind(1, get<0>(key));
+			stmt_delete.bind(2, get<1>(key));
+			stmt_delete.exec(); stmt_delete.reset();
+			continue;
+		}
+		data.erase(key);
+	}
+	stmt_select.reset();
+
+	// new records
+	for (auto &pair : data) {
+		stmt_insert.bind(1, get<0>(pair.first));
+		stmt_insert.bind(2, get<1>(pair.first));
+		stmt_insert.exec(); stmt_insert.reset();
+	}
+}
 
 } // namespace
